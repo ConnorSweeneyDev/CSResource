0ce6d6ecb206885295b4b792826c0276557a29d5
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-07-24 03:04:13 +0100

    Made a basic compiler.

--- program/source/main.cpp
+++ program/source/main.cpp
@@ -1,74 +1,149 @@
 #include <cstddef>
 #include <cstdlib>
+#include <exception>
 #include <filesystem>
 #include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 #include "SDL3/SDL_error.h"
 #include "SDL3/SDL_init.h"
 #include "SDL3/SDL_iostream.h"
-#include "SDL3/SDL_log.h"
 #include "SDL3/SDL_stdinc.h"
 #include "SDL3_shadercross/SDL_shadercross.h"
 
-int main(int argc, char *argv[])
+int try_main(int argc, char *argv[])
 {
-  if (argc > 1 || !argv)
-  {
-    std::cerr << "Usage: " << argv[0] << std::endl;
-    return EXIT_FAILURE;
-  }
+  const std::string usage =
+    "Usage: CSResource [compile <shaders> <output_directory>] | [load <resources> <output>(.hpp|.cpp)]";
 
-  if (!SDL_ShaderCross_Init())
-  {
-    SDL_LogError(SDL_LOG_CATEGORY_GPU, "%s", "Failed to initialize shadercross!");
-    return EXIT_FAILURE;
-  }
+  std::vector<std::string> args;
+  for (int index = 0; index < argc; ++index) args.emplace_back(argv[index]);
+  if (args.size() == 1) throw std::runtime_error(usage);
 
-  size_t file_size = 0;
-  char *file_data = reinterpret_cast<char *>(SDL_LoadFile("resource/shader/main.vert", &file_size));
-  if (!file_data)
-  {
-    SDL_LogError(SDL_LOG_CATEGORY_GPU, "Failed to load shader file: %s", SDL_GetError());
-    SDL_free(file_data);
-    SDL_ShaderCross_Quit();
-    SDL_Quit();
-    return EXIT_FAILURE;
-  }
+  std::string command = args.at(1);
+  if (command != "compile" && command != "load") throw std::runtime_error(usage);
 
-  SDL_ShaderCross_HLSL_Info hlsl_info = {};
-  hlsl_info.source = reinterpret_cast<char *>(file_data);
-  hlsl_info.entrypoint = "main";
-  hlsl_info.include_dir = nullptr;
-  hlsl_info.defines = nullptr;
-  hlsl_info.shader_stage = SDL_SHADERCROSS_SHADERSTAGE_VERTEX;
-  hlsl_info.enable_debug = false;
-  hlsl_info.name = nullptr;
-  hlsl_info.props = 0;
+  if (command == "compile")
+  {
+    if (args.size() < 4) throw std::runtime_error(usage);
 
-  size_t compiled_size = 0;
-  Uint8 *compiled_shader = reinterpret_cast<Uint8 *>(SDL_ShaderCross_CompileDXILFromHLSL(&hlsl_info, &compiled_size));
-  if (!compiled_shader)
-  {
-    SDL_LogError(SDL_LOG_CATEGORY_GPU, "Failed to compile shader: %s", SDL_GetError());
-    SDL_free(file_data);
-    SDL_ShaderCross_Quit();
-    SDL_Quit();
-    return EXIT_FAILURE;
+    std::filesystem::path output_directory = args.back();
+    try
+    {
+      std::filesystem::create_directories(output_directory);
+    }
+    catch (const std::filesystem::filesystem_error &error)
+    {
+      SDL_ShaderCross_Quit();
+      SDL_Quit();
+      throw std::runtime_error("Failed to create output directory " + output_directory.string() + ": " +
+                               std::string(error.what()));
+    }
+
+    std::vector<std::filesystem::path> shaders = {};
+    shaders.reserve(args.size() - 2);
+    for (size_t index = 2; index < args.size() - 1; ++index)
+    {
+      std::filesystem::path shader = args.at(index);
+      if (!std::filesystem::exists(shader) || !std::filesystem::is_regular_file(shader) ||
+          (shader.extension() != ".vert" && shader.extension() != ".frag"))
+        throw std::runtime_error("Shader does not exist or is not a valid vert or frag file: " + shader.string());
+      shaders.emplace_back(shader);
+    }
+
+    if (!SDL_ShaderCross_Init())
+      throw std::runtime_error("Failed to initialize shadercross: " + std::string(SDL_GetError()));
+
+    for (std::filesystem::path shader : shaders)
+    {
+      size_t file_size = 0;
+      char *file_data = reinterpret_cast<char *>(SDL_LoadFile(shader.string().c_str(), &file_size));
+      if (!file_data)
+      {
+        SDL_ShaderCross_Quit();
+        SDL_Quit();
+        throw std::runtime_error("Failed to load shader " + shader.string() + ": " + std::string(SDL_GetError()));
+      }
+
+      SDL_ShaderCross_HLSL_Info hlsl_info = {reinterpret_cast<char *>(file_data),
+                                             "main",
+                                             nullptr,
+                                             nullptr,
+                                             shader.extension() == ".vert" ? SDL_SHADERCROSS_SHADERSTAGE_VERTEX
+                                                                           : SDL_SHADERCROSS_SHADERSTAGE_FRAGMENT,
+                                             false,
+                                             nullptr,
+                                             0};
+      size_t compiled_dxil_size = 0;
+      Uint8 *compiled_dxil_shader =
+        reinterpret_cast<Uint8 *>(SDL_ShaderCross_CompileDXILFromHLSL(&hlsl_info, &compiled_dxil_size));
+      if (!compiled_dxil_shader)
+      {
+        SDL_ShaderCross_Quit();
+        SDL_Quit();
+        throw std::runtime_error("Failed to compile dxil shader " + shader.string() + ": " +
+                                 std::string(SDL_GetError()));
+      }
+
+      SDL_IOStream *dxil_output =
+        SDL_IOFromFile((output_directory / shader.filename()).string().append(".dxil").c_str(), "w");
+      if (!dxil_output)
+      {
+        SDL_free(compiled_dxil_shader);
+        SDL_ShaderCross_Quit();
+        SDL_Quit();
+        throw std::runtime_error("Failed to open dxil output file for shader " + shader.string() + ": " +
+                                 std::string(SDL_GetError()));
+      }
+      SDL_WriteIO(dxil_output, compiled_dxil_shader, compiled_dxil_size);
+
+      SDL_CloseIO(dxil_output);
+      SDL_free(compiled_dxil_shader);
+
+      size_t compiled_spv_size = 0;
+      Uint8 *compiled_spv_shader =
+        reinterpret_cast<Uint8 *>(SDL_ShaderCross_CompileSPIRVFromHLSL(&hlsl_info, &compiled_spv_size));
+      if (!compiled_spv_shader)
+      {
+        SDL_ShaderCross_Quit();
+        SDL_Quit();
+        throw std::runtime_error("Failed to compile spirv shader " + shader.string() + ": " +
+                                 std::string(SDL_GetError()));
+      }
+
+      SDL_IOStream *spv_output =
+        SDL_IOFromFile((output_directory / shader.filename()).string().append(".spv").c_str(), "w");
+      if (!spv_output)
+      {
+        SDL_free(compiled_spv_shader);
+        SDL_ShaderCross_Quit();
+        SDL_Quit();
+        throw std::runtime_error("Failed to open spirv output file for shader " + shader.string() + ": " +
+                                 std::string(SDL_GetError()));
+      }
+      SDL_WriteIO(spv_output, compiled_spv_shader, compiled_spv_size);
+      SDL_CloseIO(spv_output);
+      SDL_free(compiled_spv_shader);
+
+      SDL_free(file_data);
+    }
   }
-  std::filesystem::create_directories("build/Output");
-  SDL_IOStream *output = SDL_IOFromFile("build/Output/main.vert.dxil", "w");
-  if (!output)
-  {
-    SDL_LogError(SDL_LOG_CATEGORY_GPU, "Failed to open output file: %s", SDL_GetError());
-    SDL_free(file_data);
-    SDL_free(compiled_shader);
-    SDL_ShaderCross_Quit();
-    SDL_Quit();
-    return EXIT_FAILURE;
-  }
-  SDL_WriteIO(output, compiled_shader, compiled_size);
-  SDL_CloseIO(output);
 
   std::cout << "Finished!" << std::endl;
   return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[])
+{
+  try
+  {
+    return try_main(argc, argv);
+  }
+  catch (const std::exception &error)
+  {
+    std::cerr << "Exception: " << error.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 }
--- script/run.sh
+++ script/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 OUTPUT=$(find . -name "CSResource.exe")
-ARGUMENTS=
+ARGUMENTS="compile resource/shader/main.vert resource/shader/main.frag build/Output"
 
 $OUTPUT $ARGUMENTS
