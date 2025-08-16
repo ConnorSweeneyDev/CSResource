5edadfd182dd0790985eb3024dd077d97e7fa373
Connor Sweeney <connor.sweeney1234@gmail.com>
2025-08-16 19:43:27 +0100

    Using cleaner std::span + std::array rather than templates.

--- script/run.sh
+++ script/run.sh
@@ -6,6 +6,4 @@ TEXTURE_DIRECTORY="resource/texture"
 TEXTURE_FILES=$(find resource/texture -type f -name "*.png")
 OUTPUT_DIRECTORIES="build/Output/Shader build/Output/Include build/Output/Source"
 
-# $OUTPUT set $TEXTURE_FILES 50
-# $OUTPUT list $TEXTURE_DIRECTORY
 $OUTPUT compile $SHADER_FILES $TEXTURE_FILES $OUTPUT_DIRECTORIES
